9c1a98cca5dd3e15ceac96c21c91e32a35ea79d3
Rodrigo Leme <rmleme@gmail.com>
2020-04-29 00:29:51 -0300

    POST method for creating file repositories.

--- smart-transfer-server/src/main/kotlin/com/rmleme/smart/transfer/server/FileRepositoryController.kt
+++ smart-transfer-server/src/main/kotlin/com/rmleme/smart/transfer/server/FileRepositoryController.kt
@@ -1,11 +1,26 @@
 package com.rmleme.smart.transfer.server
 
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.util.UriComponentsBuilder
 
 @RestController
 class FileRepositoryController {
 
+    // For authenticated clients
     @GetMapping("/repository")
-    fun getRepositoryAccess() = "Successfully got access."
+    fun getRepository() = FileRepository(name = "Repository")
+
+    // For authenticated admin users
+    @PostMapping("/repository")
+    fun createRepository(
+        @RequestBody repository: FileRepository,
+        uriBuilder: UriComponentsBuilder
+    ): ResponseEntity<FileRepository> {
+        val uriComponents = uriBuilder.path("/repository/{id}").buildAndExpand(repository.id)
+        return ResponseEntity.created(uriComponents.toUri()).build()
+    }
 }
--- smart-transfer-server/src/test/kotlin/com/rmleme/smart/transfer/server/FileRepositoryControllerTest.kt
+++ smart-transfer-server/src/test/kotlin/com/rmleme/smart/transfer/server/FileRepositoryControllerTest.kt
@@ -2,13 +2,20 @@ package com.rmleme.smart.transfer.server
 
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
+import org.springframework.http.HttpStatus
+import org.springframework.web.util.UriComponentsBuilder
 
 class FileRepositoryControllerTest : StringSpec({
 
     val controller = FileRepositoryController()
 
-    "GET /repository should return proper response message" {
-        val response = controller.getRepositoryAccess()
-        response shouldBe "Successfully got access."
+    "GET /repository should return proper Repository" {
+        val response = controller.getRepository()
+        response.name shouldBe "Repository"
+    }
+
+    "POST /repository should return HTTP Status Code 201" {
+        val response = controller.createRepository(FileRepository(), UriComponentsBuilder.newInstance())
+        response.statusCode shouldBe HttpStatus.CREATED
     }
 })
