0f86a7eac449de6975aba1782fe15f15cc628ef7
Rodrigo Leme <rmleme@gmail.com>
2020-05-03 05:12:31 -0300

    First (dummy) implementation of a file download (with S3's TransferManager).

--- smart-transfer-client/src/main/kotlin/com/rmleme/smart/transfer/client/Client.kt
+++ smart-transfer-client/src/main/kotlin/com/rmleme/smart/transfer/client/Client.kt
@@ -1,9 +1,18 @@
 package com.rmleme.smart.transfer.client
 
+import com.amazonaws.services.s3.transfer.Download
+import com.amazonaws.services.s3.transfer.TransferManager
+import com.amazonaws.services.s3.transfer.TransferManagerBuilder
+import java.io.File
+
 class Client {
 
 }
 
-fun main() {
-    println("Hello World!")
+fun main(args: Array<String>) {
+    val transferManager: TransferManager = TransferManagerBuilder.standard().build()
+    val file = File("arquivo.txt")
+    val download: Download = transferManager.download("bucket", "key", file)
+    download.waitForCompletion()
+    transferManager.shutdownNow()
 }
