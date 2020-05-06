db6e474428dc36b4b83b7398e8e3854315f74f58
Rodrigo Leme <rmleme@gmail.com>
2020-05-06 00:30:21 -0300

    [Client] Download feature (still hardcoded).

--- smart-transfer-client/src/main/kotlin/com/rmleme/smart/transfer/client/Client.kt
+++ smart-transfer-client/src/main/kotlin/com/rmleme/smart/transfer/client/Client.kt
@@ -4,15 +4,24 @@ import com.amazonaws.services.s3.transfer.Download
 import com.amazonaws.services.s3.transfer.TransferManager
 import com.amazonaws.services.s3.transfer.TransferManagerBuilder
 import java.io.File
+import java.nio.file.Files
 
-class Client {
+private const val ROOT_DIRECTORY = "/tmp"
 
+class Client(private val bucket: String) {
+
+    fun download(key: String): File {
+        val transferManager: TransferManager = TransferManagerBuilder.standard( ).build()
+        val file = File("$ROOT_DIRECTORY/$key")
+        val download: Download = transferManager.download(bucket, key, file)
+        download.waitForCompletion()
+        transferManager.shutdownNow()
+        return file
+    }
 }
 
 fun main(args: Array<String>) {
-    val transferManager: TransferManager = TransferManagerBuilder.standard().build()
-    val file = File("arquivo.txt")
-    val download: Download = transferManager.download("bucket", "key", file)
-    download.waitForCompletion()
-    transferManager.shutdownNow()
+    val client = Client("smart-transfer-bucket")
+    val file = client.download("download/arquivo.txt")
+    Files.lines(file.toPath()).forEach { print(it) }
 }
