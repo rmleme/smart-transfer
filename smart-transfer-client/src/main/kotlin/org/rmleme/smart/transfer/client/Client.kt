08c912540b0f9e405156931bd7bab0d5e00d205a
Rodrigo Leme <rmleme@gmail.com>
2020-09-07 18:05:57 -0300

    [Client] Added logging

--- smart-transfer-client/src/main/kotlin/org/rmleme/smart/transfer/client/Client.kt
+++ smart-transfer-client/src/main/kotlin/org/rmleme/smart/transfer/client/Client.kt
@@ -2,6 +2,7 @@ package org.rmleme.smart.transfer.client
 
 import com.amazonaws.services.s3.transfer.TransferManager
 import com.amazonaws.services.s3.transfer.TransferManagerBuilder
+import org.slf4j.LoggerFactory
 import java.io.Closeable
 import java.io.File
 import java.nio.file.Files
@@ -14,16 +15,24 @@ class Client(private val fileRepositoryName: String) : Closeable {
     private val transferManager: TransferManager = TransferManagerBuilder.standard().build()
 
     fun download(file: File) {
+        logger.info("Starting download of ${file.name}...")
         val download = transferManager.download(fileRepositoryName, "$DOWNLOAD_FOLDER/${file.name}", file)
         download.waitForCompletion()
+        logger.info("Download completed.")
     }
 
     fun upload(file: File) {
+        logger.info("Starting upload of ${file.name}...")
         val upload = transferManager.upload(fileRepositoryName, "$UPLOAD_FOLDER/${file.name}", file)
         upload.waitForCompletion()
+        logger.info("Upload completed.")
     }
 
     override fun close() = transferManager.shutdownNow()
+
+    companion object {
+        private val logger = LoggerFactory.getLogger(Client::class.java)
+    }
 }
 
 fun main(args: Array<String>) {
