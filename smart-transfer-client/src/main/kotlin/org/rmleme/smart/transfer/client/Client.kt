bf08014f92edd183e8c1dd4efb0ada8eb49e3d19
Rodrigo Leme <rmleme@gmail.com>
2020-09-07 17:34:09 -0300

    Client is now Cloaseable

--- smart-transfer-client/src/main/kotlin/org/rmleme/smart/transfer/client/Client.kt
+++ smart-transfer-client/src/main/kotlin/org/rmleme/smart/transfer/client/Client.kt
@@ -2,36 +2,40 @@ package org.rmleme.smart.transfer.client
 
 import com.amazonaws.services.s3.transfer.TransferManager
 import com.amazonaws.services.s3.transfer.TransferManagerBuilder
+import java.io.Closeable
 import java.io.File
 import java.nio.file.Files
 
-class Client(private val fileRepositoryName: String) {
+private const val DOWNLOAD_FOLDER = "downloads"
+private const val UPLOAD_FOLDER = "uploads"
+
+class Client(private val fileRepositoryName: String) : Closeable {
 
     private val transferManager: TransferManager = TransferManagerBuilder.standard().build()
 
-    fun download(key: String): File {
-        val file = File("/tmp/$key")
-        val download = transferManager.download(fileRepositoryName, key, file)
+    fun download(file: File) {
+        val download = transferManager.download(fileRepositoryName, "$DOWNLOAD_FOLDER/${file.name}", file)
         download.waitForCompletion()
-        transferManager.shutdownNow()
-        return file
     }
 
     fun upload(file: File) {
-        val upload = transferManager.upload("uploads/$fileRepositoryName", file.name, file)
+        val upload = transferManager.upload(fileRepositoryName, "$UPLOAD_FOLDER/${file.name}", file)
         upload.waitForCompletion()
-        transferManager.shutdownNow()
     }
+
+    override fun close() = transferManager.shutdownNow()
 }
 
 fun main(args: Array<String>) {
     val client = Client("smart-transfer-bucket")
 
-    var file = File("/tmp/arquivo.txt")
-    file.writeText("Hello World!")
+    client.use {
+        val fileToUpload = File("${System.getProperty("user.home")}/arquivo_upload.txt")
+        fileToUpload.writeText("[Uploads]\nHello World!")
+        client.upload(fileToUpload)
 
-    client.upload(file)
-
-    file = client.download("downloads/arquivo.txt")
-    Files.lines(file.toPath()).forEach { print(it) }
+        val fileToDownload = File("${System.getProperty("user.home")}/arquivo_download.txt")
+        client.download(fileToDownload)
+        Files.lines(fileToDownload.toPath()).forEach { println(it) }
+    }
 }
