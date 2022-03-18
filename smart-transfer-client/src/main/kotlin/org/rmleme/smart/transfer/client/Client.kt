2dcf78baeaef3f10ad3c452ecd1dd434cbac2706
Rodrigo Leme <rmleme@gmail.com>
2022-03-18 03:22:55 -0300

    [Client] Handle download crashes

--- smart-transfer-client/src/main/kotlin/org/rmleme/smart/transfer/client/Client.kt
+++ smart-transfer-client/src/main/kotlin/org/rmleme/smart/transfer/client/Client.kt
@@ -1,22 +1,71 @@
 package org.rmleme.smart.transfer.client
 
-import com.amazonaws.services.s3.transfer.TransferManager
+import com.amazonaws.event.ProgressEvent
+import com.amazonaws.services.s3.model.GetObjectRequest
+import com.amazonaws.services.s3.transfer.Download
+import com.amazonaws.services.s3.transfer.PersistableDownload
+import com.amazonaws.services.s3.transfer.PersistableTransfer
 import com.amazonaws.services.s3.transfer.TransferManagerBuilder
+import com.amazonaws.services.s3.transfer.internal.S3SyncProgressListener
 import org.slf4j.LoggerFactory
 import java.io.Closeable
 import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
 
 private const val DOWNLOAD_FOLDER = "downloads"
 private const val UPLOAD_FOLDER = "uploads"
 
 class Client(private val fileRepositoryName: String) : Closeable {
 
-    private val transferManager: TransferManager = TransferManagerBuilder.standard().build()
+    private val transferManager = TransferManagerBuilder.standard().build()
+
+    private val progressListener = object : S3SyncProgressListener() {
+        override fun progressChanged(progressEvent: ProgressEvent) {
+            logger.info("Bytes transferred: ${progressEvent.bytesTransferred}")
+        }
+
+        override fun onPersistableTransfer(persistableTransfer: PersistableTransfer) {
+            val downloadStateFile = File(System.getProperty("user.home") + "/download-state")
+            if (!downloadStateFile.exists()) {
+                downloadStateFile.createNewFile()
+            }
+            FileOutputStream(downloadStateFile).use { persistableTransfer.serialize(it) }
+        }
+    }
 
     fun download(file: File) {
+        val downloadStateFile = File(System.getProperty("user.home") + "/download-state")
+        if (!downloadStateFile.exists()) {
+            startNewDownload(file, downloadStateFile)
+        } else {
+            resumeDownload(file, downloadStateFile)
+        }
+    }
+
+    private fun startNewDownload(file: File, downloadStateFile: File) {
         logger.info("Starting download of ${file.name}...")
-        val download = transferManager.download(fileRepositoryName, "$DOWNLOAD_FOLDER/${file.name}", file)
+        val getObjectRequest = GetObjectRequest(fileRepositoryName, "$DOWNLOAD_FOLDER/${file.name}")
+        val download = transferManager.download(getObjectRequest, file, progressListener)
+        completeDownload(download, downloadStateFile)
+    }
+
+    private fun resumeDownload(file: File, downloadStateFile: File) {
+        logger.info("Resuming download of ${file.name}...")
+        FileInputStream(downloadStateFile).use {
+            val persistableDownload = PersistableDownload.deserializeFrom<PersistableDownload>(it)
+            val download = transferManager.resumeDownload(persistableDownload)
+            download.addProgressListener(progressListener)
+            completeDownload(download, downloadStateFile)
+        }
+    }
+
+    private fun completeDownload(download: Download, downloadStateFile: File) {
         download.waitForCompletion()
+        transferManager.shutdownNow()
+        if (download.isDone) {
+            downloadStateFile.delete()
+        }
         logger.info("Download completed.")
     }
 
@@ -38,11 +87,11 @@ fun main(args: Array<String>) {
     val client = Client("smart-transfer-bucket")
 
     client.use {
-        val fileToUpload = File("${System.getProperty("user.home")}/arquivo_upload.txt")
+        /*val fileToUpload = File("${System.getProperty("user.home")}/arquivo_upload.txt")
         if (!fileToUpload.exists()) {
             fileToUpload.writeText("[Uploads]\nHello World!")
         }
-        client.upload(fileToUpload)
+        client.upload(fileToUpload)*/
 
         val fileToDownload = File("${System.getProperty("user.home")}/arquivo_download.txt")
         client.download(fileToDownload)
